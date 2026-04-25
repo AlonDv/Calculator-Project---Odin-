07b56274846531b08348de31c7c8d55aaab10960
Alon <217150097+AlonDv@users.noreply.github.com>
2026-04-25 13:04:50 -0700

    Add basic calculator operation code(without checks)

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+const container = document.querySelector(".container");
+const display = document.querySelector(".display");
+
 function add(num1, num2){
     return num1 + num2; 
 }
@@ -18,10 +21,11 @@ function divide(num1, num2){
     return num1 / num2; 
 }
 
-let firstNum = 10;
-let operator = "+"; 
-let secondNum = 10; 
+let firstNum = "";
+let operator = ""; 
+let secondNum = ""; 
 
+container.addEventListener("click", assign);
 
 function operate(operator, firstNum, secondNum){
     switch(operator){
@@ -40,5 +44,51 @@ function operate(operator, firstNum, secondNum){
     }   
 }
 
+
+
+function assign(event){
+    if(event.target.tagName == "BUTTON"){
+        let button = event.target; 
+        let value = button.value; 
+        console.log(value); 
+        
+        
+        switch(true){
+            case button.classList.contains("numBtn"):
+                if (!operator){
+                    firstNum += value; 
+                } 
+
+                else if(firstNum && operator){
+                    secondNum += value; 
+                }
+                 
+                break;
+                
+
+            case button.classList.contains("redBtn"):
+                if(!operator && firstNum){
+                    operator = value; 
+                }break;
+        }  
+        display.textContent = `${firstNum} ${operator} ${secondNum}`; 
+    }
+
+    if(firstNum && operator && secondNum){
+        console.log(`${firstNum} ${operator} ${secondNum}`); 
+        console.log(operate(operator, +firstNum, +secondNum));
+    }
+        
+}
+
+
+const equalBtn = document.querySelector("#equalBtn")
+equalBtn.addEventListener("click", ()=>{
+    let result = operate(operator, +firstNum, +secondNum);
+    firstNum = result;
+    secondNum = ""
+    operator = ""; 
+    display.textContent = `${result}`; 
+})
 
 console.log(operate()); 
