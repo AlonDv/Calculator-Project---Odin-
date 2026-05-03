c35eb8bdde7e521f4472b93daba100235d059d7c
Alon <217150097+AlonDv@users.noreply.github.com>
2026-05-03 12:42:00 -0700

    Complete Odin Project Calculator Project

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const calculation = {
     secondNum: "",
     firstNumWasResult : false, 
     isInfinity: false, 
-    dotIsCurrent: false, 
+    dotInFirstNum : false, 
+    dotInSecondNum : false, 
 
     add(){
         return (+this.firstNum + +this.secondNum)
@@ -56,7 +57,8 @@ const calculation = {
                 break;
         }
 
-        return (Number.isFinite(result) ? result : "Nice Try!");
+        
+        return (Number.isFinite(result) ? ((Number.isInteger(result)) ? result : result.toFixed(2)) : "Nice Try!");
     }
 
 }
@@ -72,9 +74,7 @@ function assign(event){
         let value = button.value;
 
             if(button.classList.contains("numBtn")){
-
-
-
+                
                 if(calculation.isInfinity || calculation.firstNumWasResult && !calculation.operator){
                     calculation.isInfinity = false; 
                     calculation.firstNumWasResult = false; 
@@ -82,12 +82,24 @@ function assign(event){
                     calculation.secondNum = "";
                     calculation.operator = "";
                 }
+
+
                 else if (!calculation.operator && !calculation.firstNumWasResult && !calculation.isInfinity){
-                    calculation.firstNum += value; 
+                    if(value === "." && calculation.firstNum.includes(".")){
+                        return;
+                    }
+                    else{
+                        calculation.firstNum += value
+                        calculation.dotInFirstNum = true; 
+                    };  
+                    
                 } 
 
                 else if(calculation.firstNum && calculation.operator && !calculation.isInfinity){
-                    calculation.secondNum += value; 
+                     if(value === "." && calculation.secondNum.includes(".")){
+                        return;
+                    }
+                     else{calculation.secondNum += value}; 
                 }
                 display.textContent = `${calculation.firstNum} ${calculation.operator} ${calculation.secondNum}`; 
             }
@@ -101,7 +113,7 @@ function assign(event){
                 }
                 else if( calculation.firstNum && calculation.operator && calculation.secondNum){
                     const result = calculation.operate();
-                    if(typeof result === "string"){
+                    if(typeof +result === "NaN"){
                         display.textContent = result;
                         calculation.firstNum = "";
                         calculation.secondNum = "";
@@ -135,7 +147,7 @@ equalBtn.addEventListener("click", ()=>{
     calculation.firstNumWasResult = true; 
     display.textContent = `${result}`; 
 
-    if(typeof result === "string"){
+    if(typeof +result === "NaN"){
         calculation.isInfinity = true;
     }
 })
@@ -181,171 +193,3 @@ clearBtn.addEventListener("click" , ()=>{
 
 
 
-
-
-
-// const container = document.querySelector(".container");
-// const display = document.querySelector(".display");
-
-// function add(num1, num2){
-//     return num1 + num2; 
-// }
-
-
-// function subtract(num1, num2){
-//     return num1 - num2; 
-// }
-
-
-// function multiply(num1, num2){
-//     return num1 * num2; 
-// }
-
-
-// function divide(num1, num2){
-//     return num1 / num2; 
-// }
-
-// function toPower(num1, num2){
-//     return num1 ** num2;
-// }
-
-// let firstNum = "";
-// let operator = ""; 
-// let secondNum = ""; 
-// let wasResult = false; 
-// let allowButtons = true; 
-
-
-// container.addEventListener("click", assign);
-
-// function operate(operator, firstNum, secondNum){
-//     let result = null; 
-//     switch(operator){
-//         case "+":
-//             result = add(+firstNum,+secondNum);
-//             break; 
-        
-//         case "-":
-//             result =  subtract(+firstNum,+secondNum); 
-//             break; 
-
-//         case "*":
-//             result = multiply(+firstNum, +secondNum);
-//             break; 
-//         case "/":
-//             result =  divide(+firstNum, +secondNum); 
-//             break;
-
-//         case "^":
-//             result = toPower(+firstNum, +secondNum); 
-//             break;
-        
-//     }   
-
-//     if(!(Number.isFinite(result))){
-//         result = "Nice Try";
-//         firstNum = ""; 
-//         secondNum = "";
-//         operator = ""; 
-//         display.textContent = result;
-//         allowButtons = false; 
-//     }
-
-//     else{
-//         return result = (Number.isInteger(result) ? result : result.toFixed(2)); 
-//     }
-    
-// }
-
-
-
-
-
-// function assign(event){
-//     if(event.target.tagName == "BUTTON"){
-//         let button = event.target; 
-//         let value = button.value; 
-
-//             if(button.classList.contains("numBtn")) {
-//                 allowButtons = true; 
-//                  if (!operator && allowButtons){
-//                     firstNum += value; 
-//                 } 
-
-//                 else if(firstNum && operator && allowButtons){
-//                     secondNum += value; 
-//                 }
-
-//                 display.textContent = `${firstNum} ${operator} ${secondNum}`;
-//             }
-               
-                
-                
-
-//             else if(button.classList.contains("redBtn")){
-//                 if(!operator && firstNum && allowButtons){
-//                     operator = value;
-//                 }
-//                  else if(operator && secondNum && allowButtons){
-//                     const result = operate(operator, firstNum, secondNum);
-//                     wasResult = true; 
-//                     display.textContent = result; 
-//                     }
-
-//                     else{
-//                         operator = button.value; 
-//                         firstNum = result;
-//                         secondNum = "";
-//                         display.textContent = `${firstNum} ${operator} ${secondNum}`;
-//                     }
-                    
-//                 }
-
-                
-//             } 
-
-//             else return; 
-
-
-                
-        
-
-        
-
-        
-//     } 
-    
-
-
-
-// const equalBtn = document.querySelector("#equalBtn")
-// equalBtn.addEventListener("click", ()=>{
-//     if(firstNum && operator && secondNum){
-//         let result = (operate(operator, firstNum, secondNum));
-//         if(typeof result === "string"){
-//             display.textContent = result;
-//             secondNum = ""; 
-//             operator = ""; 
-//             firstNum = ""; 
-//         }
-
-//         else{
-//             firstNum = result;
-//             secondNum = ""
-//             operator = ""; 
-//             display.textContent = `${firstNum}`; 
-            
-//         }
-
-//     }
-
-// })
-
-// const clearBtn = document.querySelector("#clearBtn");
-// clearBtn.addEventListener("click",()=>{
-//     firstNum = "";
-//     secondNum = "";
-//     operator = ""; 
-//     display.textContent = ""; 
-// }) 
